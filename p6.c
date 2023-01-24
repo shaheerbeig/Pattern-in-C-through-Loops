f16752715104434f9671b2efc9f9454b76e207fc
Shaheer Beig <112177155+shaheerbeig@users.noreply.github.com>
2023-01-24 06:54:29 -0800

    Add files via upload

---
+++ p6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int i,j,k;
+	for(i=0;i<5;i++){
+		for(k=4;k>i;k--){
+			printf(" ");
+		}
+		for(j=0;j<=i;j++){
+			printf("* ");
+		}
+		printf("\n");
+	}
+}
---
+++ p8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int i , j , k;
+	for(i=1;i<=5;i++){
+		for(k=2;k<=i;k++){
+			printf(" ");
+		}
+		for(j=10;j>2*i-1;j--){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
